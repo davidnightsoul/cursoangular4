4a12b4c144c08730981a1e660023f512e6abbf1e
davidnightsoul <david.barrionuevo@gmail.com>
2017-07-31 16:04:01 -0300

    15-Ejercicio(Soportar diferentes llamada s a servicios Rest API)

--- src/app/shared/services/user.service.ts
+++ src/app/shared/services/user.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Http, Headers, RequestOptions } from '@angular/http';
 import 'rxjs';
 import { User } from '../models/user';
+import { environment } from '../../../environments/environment'
 
 const SERVER_REST_API_URL = "http://localhost:3000/users/";
 
@@ -9,13 +10,15 @@ const SERVER_REST_API_URL = "http://localhost:3000/users/";
 export class UserService {
 
  private http: Http;
+ private serverRestAPIUrl: string;
 
  constructor(http:Http) {
    this.http = http;
+   this.serverRestAPIUrl = environment.apiEndPoint + "/users";
  }
 
  getUsers() {
-   return this.http.get(SERVER_REST_API_URL)
+   return this.http.get(this.serverRestAPIUrl)
        .map(res => res.json());
  }
 
@@ -27,11 +30,11 @@ export class UserService {
    });
    let headers = new Headers({ 'Content-Type': 'application/json' });
    let options = new RequestOptions({ headers: headers });
-   return this.http.post(SERVER_REST_API_URL, body, options);
+   return this.http.post(this.serverRestAPIUrl, body, options);
  }
 
   deleteUser(id) {
-   return this.http.delete(SERVER_REST_API_URL + id);
+   return this.http.delete(this.serverRestAPIUrl + id);
  }
 
 }
